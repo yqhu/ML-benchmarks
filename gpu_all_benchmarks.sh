a09279fda59c071bd61f0d7bcc90f6c4705ec5c8
Patrick Hu <patrickhu@fb.com>
2022-02-24 00:19:19 +0000

    Update gpu_all_benchmarks.sh for g4dn

--- gpu_all_benchmarks.sh
+++ gpu_all_benchmarks.sh
@@ -1,19 +1,19 @@
 PREFIX=g4dn
 
-python benchmark_runs.py --model_path bert-base-uncased --backend eager --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 8 16 32 64 128 --gpu --prefix %PREFIX
-python benchmark_runs.py --model_path bert-base-uncased --backend eager --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 8 16 32 64 128 --gpu --fp16 --prefix %PREFIX
+python benchmark_runs.py --model_path bert-base-uncased --backend eager --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 8 16 32 64 128 --gpu --prefix $PREFIX
+python benchmark_runs.py --model_path bert-base-uncased --backend eager --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 8 16 32 64 128 --gpu --fp16 --prefix $PREFIX
 
-python benchmark_runs.py --model_path model.pt --backend torchscript --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 8 16 32 64 128 --gpu --prefix %PREFIX
-python benchmark_runs.py --model_path model_fp16.pt --backend torchscript --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 8 16 32 64 128 --gpu --fp16 --prefix %PREFIX
+python benchmark_runs.py --model_path model.pt --backend torchscript --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 8 16 32 64 128 --gpu --prefix $PREFIX
+python benchmark_runs.py --model_path model_fp16.pt --backend torchscript --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 8 16 32 64 128 --gpu --fp16 --prefix $PREFIX
 
-python benchmark_runs.py --model_path model.onnx --backend ort --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 8 16 32 64 128 --gpu --prefix %PREFIX
-#python benchmark_runs.py --model_path model_fp16.onnx --backend ort --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 8 16 32 64 128 --gpu --fp16 --prefix %PREFIX
+python benchmark_runs.py --model_path model.onnx --backend ort --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 8 16 32 64 128 --gpu --prefix $PREFIX
+#python benchmark_runs.py --model_path model_fp16.onnx --backend ort --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 8 16 32 64 128 --gpu --fp16 --prefix $PREFIX
 
-python benchmark_runs.py --model_path cv_eager.pt --backend cv_eager --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 64 128 224 --gpu --prefix %PREFIX
-python benchmark_runs.py --model_path cv_eager.pt --backend cv_eager --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 64 128 224 --gpu --fp16 --prefix %PREFIX
+python benchmark_runs.py --model_path cv_eager.pt --backend cv_eager --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 64 128 224 --gpu --prefix $PREFIX
+python benchmark_runs.py --model_path cv_eager.pt --backend cv_eager --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 64 128 224 --gpu --fp16 --prefix $PREFIX
 
-python benchmark_runs.py --model_path cv_ts.pt --backend cv_torchscript --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 64 128 224 --gpu --prefix %PREFIX
-python benchmark_runs.py --model_path cv_ts.pt --backend cv_torchscript --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 64 128 224 --gpu --fp16 --prefix %PREFIX
+python benchmark_runs.py --model_path cv_ts.pt --backend cv_torchscript --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 64 128 224 --gpu --prefix $PREFIX
+python benchmark_runs.py --model_path cv_ts.pt --backend cv_torchscript --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 64 128 224 --gpu --fp16 --prefix $PREFIX
 
-python benchmark_runs.py --model_path cv_onnx.onnx --backend cv_ort --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 64 128 224 --gpu --prefix %PREFIX
-python benchmark_runs.py --model_path cv_onnx_fp16.onnx --backend cv_ort --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 64 128 224 --gpu --fp16 --prefix %PREFIX
+python benchmark_runs.py --model_path cv_onnx.onnx --backend cv_ort --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 64 128 224 --gpu --prefix $PREFIX
+python benchmark_runs.py --model_path cv_onnx_fp16.onnx --backend cv_ort --output_path . --batch_sizes 1 2 4 8 12 16 32 64 --sequence_lengths 64 128 224 --gpu --fp16 --prefix $PREFIX
